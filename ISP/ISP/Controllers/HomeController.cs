df947c91822e1c8a294882e53169e4fb1f3abd60
Juan Rojas <15171439@itculiacan.edu.mx>
2018-05-15 22:36:49 -0600

    Prueba ISPG

---
+++ ISP/ISP/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using ISP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISP.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProyectoISEntities db = new ProyectoISEntities();
+        
+        public ActionResult Index()
+        {
+            return View(db.Proyecto.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
